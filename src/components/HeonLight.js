82f00402a1922f8c30abb4266c654302f80e7a5c
guillaume Cormier <cormier.guillaume83@gmail.com>
2019-12-21 08:21:43 +0100

    sauvegarde du 21_11_2019

--- src/components/HeonLight.js
+++ src/components/HeonLight.js
@@ -1,57 +1,84 @@
 import React, {Component} from "react";
-import { HuePicker } from 'react-color'
+import {HuePicker} from 'react-color'
 
 
-
-
-
-var allume = false
 
 class HeonLight extends Component {
 
+
     constructor(props) {
         super();
+        var timer = null
+        var longClcik = false
+        var allume = false
+
+
         this.state = {
-            nom : props.nom,
-            data : props.data,
-            nblight : props.nblight,
+            nom: props.nom,
+            data: props.data,
+            nblight: props.nblight,
             color: '#fff'
+
         }
+
         this.LightClick = this.LightClick.bind(this)
+        this.MouseUp = this.MouseUp.bind(this)
+        this.MouseDown = this.MouseDown.bind(this)
     }
 
     render() {
-        var data = { ... this.state.data}
-        var nom = { ... this.state.nom}
-        var nblight = { ... this.state.nblight}
+        var data = {...this.state.data}
+        var nom = {...this.state.nom}
+        var nblight = {...this.state.nblight}
 
 
         //console.log(data)
         return (
-            <button className="btn btn-danger"
-                    //onClick={this.LightClick}
-                >
-                {this.state.nom} - {this.state.nblight}<HuePicker color={this.state.color}  onChange={this.handleChangeComplete }/>
+            <button className="btn btn-danger" onMouseDown={this.MouseDown} onMouseUp={this.MouseUp}>
+                {this.state.nom} - {this.state.nblight}
+                <HuePicker color={this.state.color}  onChange={this.handleChangeComplete }/>
+
             </button>
-            )
+        )
 
     }
-    handleChangeComplete = (color, event)=>{
+
+    MouseDown() {
+        this.longClcik = false
+        console.log("Debut du timer")
+        this.timer = setTimeout(() => {
+            console.log("Fin timer")
+            this.longClcik = true
+        }, 1000)
+
+    }
+
+    MouseUp() {
+        clearTimeout(this.timer)
+        if (this.longClcik) {
+            console.log("COOOL")
+
+        } else {
+            this.LightClick()
+        }
+    }
+
+
+    handleChangeComplete = (color, event) => {
         //console.log(color.rgb)
-        this.setState({ color: color.rgb });
-        var data = { ... this.state.data.data}
-        var color = { ... this.state.color}
+        this.setState({color: color.rgb});
+        var data = {...this.state.data.data}
+        var color = {...this.state.color}
         // var allume = { ... this.state.allume}
-        var light =  Object.keys(data).map((i) => {
+        var light = Object.keys(data).map((i) => {
             //PIXEL
-            var pixel = { ... data[i].data}
+            var pixel = {...data[i].data}
             //console.log(pixel)
             Object.keys(pixel).map((ii) => {
                     pixel[ii].g = color.g
-                    pixel[ii].r =  color.r
-                    pixel[ii].b =  color.b
+                    pixel[ii].r = color.r
+                    pixel[ii].b = color.b
                 }
-
             )
         })
 
@@ -60,53 +87,37 @@ class HeonLight extends Component {
 
 
     LightClick() {
-        var data = { ... this.state.data.data}
-        var color = { ... this.state.color}
-       // var allume = { ... this.state.allume}
-        var light =  Object.keys(data).map((i) => {
-           //PIXEL
-           var pixel = { ... data[i].data}
-           //console.log(pixel)
-           Object.keys(pixel).map((ii) => {
-               pixel[ii].g = allume ? 0 : color.g
-               pixel[ii].r = allume  ? 0 : color.r
-               pixel[ii].b = allume  ? 0 : color.b
-               }
-
-           )
+        var data = {...this.state.data.data}
+        var color = {...this.state.color}
+        // var allume = { ... this.state.allume}
+        var light = Object.keys(data).map((i) => {
+            //PIXEL
+            var pixel = {...data[i].data}
+            //console.log(pixel)
+            Object.keys(pixel).map((ii) => {
+                    pixel[ii].g = this.allume ? 0 : color.g
+                    pixel[ii].r = this.allume ? 0 : color.r
+                    pixel[ii].b = this.allume ? 0 : color.b
+                }
+            )
         })
-       // console.log(allume)
-        allume = !allume
+        console.log(this.allume)
+        this.allume = !this.allume
         this.PostData(this.state.data)
-
-
-
-
-
 
     }
 
-    PostData(data){
-         //console.log(JSON.stringify(data))
-         let request = new XMLHttpRequest();
-         //console.log("Request POST");
-         request.open("POST", "http://192.168.0.13:8080/heon");
-         //request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-         request.send(JSON.stringify(data))
-
-        // axios({
-        //     method: "post",
-        //     url: "http://192.168.0.13:8080/heon",
-        //     body: {
-        //         id : "COOOL ID"}
-        //
-        //
-        // })
+    PostData(data) {
+        //console.log(JSON.stringify(data))
+        let request = new XMLHttpRequest();
+        //console.log("Request POST");
+        request.open("POST", "http://192.168.0.13:8080/heon");
+        //request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        request.send(JSON.stringify(data))
 
     }
 
 }
-
 
 
 export default HeonLight;
